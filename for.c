3098ebd555228dc4fe0a27978b18074e2a83cc3f
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-22 17:49:11 +0530

    Create for.c

---
+++ for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int num,i,sum=0;
+ printf("enter the integer value");
+ scanf("%d",&num);
+ for(i=0;i<=n;i++)
+ {
+  sum=sum+1;
+  }
+ printf("sum of natural number");
+ return 0;
+ }
+ 
