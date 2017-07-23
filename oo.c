9c67b709361d55dd938ad4ccd0460cf071c2ab88
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-23 09:49:00 +0530

    Create oo.c

---
+++ oo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main();
+{
+ int low,high,i,flag;
+ printf("enter the values");
+ scanf("%d%d",&low,&high);
+ while(low<high)
+ {
+ flag=0;
+ for(i=1;i<=low/2;i++)
+ {
+  if(low%2==0)
+  {
+     flag=1;
+     break;
+     }
+     if(flag==0)
+     {
+      printf("enter the low num");
+      }
+      return 0;
+      }
+      }
+      }
+      
