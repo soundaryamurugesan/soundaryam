334a25cfccb66d903e34eadd9f581c7f876d9b97
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-23 14:21:03 +0530

    Create bh.c

---
+++ bh.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+	void main()
+
+	{
+
+	     int i,n;
+
+	 
+
+	     printf("Enter any number \n");
+
+	     scanf(" %d ",&n);
+
+	 
+
+	     printf("even numbers up to inputted number are - \n");
+
+	     for(i=2;i<=n;i+=2)
+
+	     {
+
+	             printf(" %d \t",i);
+
+	     }
+
+	     getch();
+
+	     }
+
