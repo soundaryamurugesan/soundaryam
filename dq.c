ebe07cc659f05539998f2c43bdc21c6530f8a102
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-23 13:40:13 +0530

    Create dq.c

---
+++ dq.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ int low,high,i,flag;
+ printf("enter the number");
+ scanf("%d%d",&low,&high);
+ while(low<high)
+ {
+  flag=0;
+  for(i=1;i<=low/2;i++)
+  {
+   if(low%2==0)
+   {
+    flag=1;
+    break;
+    }
+    if(flag==0)
+    {
+     printf("enter the low");
+     }
+     return 0;
+     }
+     }
+     }
