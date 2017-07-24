faed666f132e70a6171ad724ea0d871c327a1b7c
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-24 13:35:46 +0530

    Create wrw.c

---
+++ wrw.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ charc;
+ printf("enter the character");
+ scanf("%c",&c);
+ if(c>='a'&&c<='z')
+ printf("%c  is a alphabet:");
+ else
+ printf("%c is not a alphabet:");
+ return 0;
+ }
