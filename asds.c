a3a4cb3d9757b1b82a7090e1073c59d3136c1016
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-24 15:45:31 +0530

    Create asds.c

---
+++ asds.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int i,n,t1=0,t2=1,next term;
+ printf("enter the number of terms");
+ scanf("%d",&n);
+ printf("fibonacci series");
+ for(i=1;i<=n;i++)
+ {
+ printf("%d",t1);
+ next term=t1+t2;
+ t1=t2;
+ t2=next term;
+ }
+ return 0;
+ }
