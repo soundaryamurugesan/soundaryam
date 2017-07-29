b0ba0fb81886c5a7f653a2c55e06aa6613fd8fe8
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-29 16:52:10 +0530

    Create segfv.c

---
+++ segfv.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main
+{
+char string1[10],string[10];
+int i,temp=0;
+printf("enter the string1 value");
+get(string1);
+printf("enter the string2 value");
+get(string2);
+for(i=0;string1[i]!='\0';i++)
+{
+if(string1[i]=string2[i])
+ temp=1;
+ else
+ temp=0;
+ }
+ if(temp==1)
+ {
+  printf("enter both string are same");
+  else
+  printf("enter both string are not same");
+  getch();
+  }
+  }
